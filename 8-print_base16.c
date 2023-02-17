2523aa1f54cab5952cb1c802ceb661ccd9bc169f
peterjoooo <peterman471@gmail.com>
2023-02-17 15:12:43 -0800

    base16

---
+++ 8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{int i;
+int j;
+for (i = 0 ; i < 10 ; i++)
+	putchar(i + '0');
+		for (j = 'a' ; j <= 'f' ; i++)
+			putchar(j);
+		putchar('\n');
+		return (0);
+}		
+
